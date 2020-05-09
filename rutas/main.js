30406ef1d2f46d94210e104a0b8b3f899b2cddc7
Jorge <yBoQyUyz?)Yf>
2020-05-09 12:57:31 -0500

    express server

--- rutas/main.js
+++ rutas/main.js
@@ -1,7 +1,7 @@
 const router = app => {
     app.get('/', (request, response) => {
         console.log('URL: ' + request.url);
-        response.send({mensaje: 'Hola camarada'});
+        response.send({mensaje: 'Hola camarada...'});
     })
 }
 
